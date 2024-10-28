8124229ef296fd50d4072de6689ded48adebf6f1
missolin <109609110+missolin@users.noreply.github.com>
2024-10-28 21:43:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // 指定输出目录为 dist
+  },
 });
